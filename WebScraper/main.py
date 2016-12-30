dcc78bb4ccd39d6fe0a9f7c7423338644bc61e4e
Bojan Komazec <Bojan Komazec>
2016-12-30 22:46:57 +0000

    Existing code moved into functions. python path removed from workspace settings (moved to user settings which are not tracked)

--- WebScraper/main.py
+++ WebScraper/main.py
@@ -5,11 +5,17 @@ Original code: http://docs.python-guide.org/en/latest/scenarios/scrape/
 from lxml import html
 import requests
 
-PAGE = requests.get('http://econpy.pythonanywhere.com/ex/001.html')
-TREE = html.fromstring(PAGE.content)
-#This will create a list of buyers:
-BUYERS = TREE.xpath('//div[@title="buyer-name"]/text()')
-#This will create a list of prices
-PRICES = TREE.xpath('//span[@class="item-price"]/text()')
-print('Buyers: ', BUYERS)
-print('Prices: ', PRICES)
+def example1():
+    response = requests.get('http://econpy.pythonanywhere.com/ex/001.html')
+    TREE = html.fromstring(response.content)
+    #This will create a list of buyers:
+    BUYERS = TREE.xpath('//div[@title="buyer-name"]/text()')
+    #This will create a list of prices
+    PRICES = TREE.xpath('//span[@class="item-price"]/text()')
+    print('Buyers: ', BUYERS)
+    print('Prices: ', PRICES)
+
+def main():
+    example1()
+
+main()
