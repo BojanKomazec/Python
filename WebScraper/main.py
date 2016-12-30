daece88d47bb001838581590df50ef99969c50fc
Bojan Komazec <bojan.komazec@gmail.com>
2016-12-30 14:31:02 +0000

    Code updated

--- WebScraper/main.py
+++ WebScraper/main.py
@@ -1,1 +1,15 @@
-print("Hello, world")
+'''
+Web page scraping example.
+Original code: http://docs.python-guide.org/en/latest/scenarios/scrape/
+'''
+from lxml import html
+import requests
+
+PAGE = requests.get('http://econpy.pythonanywhere.com/ex/001.html')
+TREE = html.fromstring(PAGE.content)
+#This will create a list of buyers:
+BUYERS = TREE.xpath('//div[@title="buyer-name"]/text()')
+#This will create a list of prices
+PRICES = TREE.xpath('//span[@class="item-price"]/text()')
+print('Buyers: ', BUYERS)
+print('Prices: ', PRICES)
